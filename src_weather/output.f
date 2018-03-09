3176228d45e13f9f9f7f65c976ee59f9d20dcbe7
Ophelia Gunn <offg@papc-astlab-11.st-andrews.ac.uk>
2018-03-09 11:43:06 +0000

    changed Plot_logp to include vdrift and bmix. Added Plot_tdep which plots column density vs time

--- src_weather/output.f
+++ src_weather/output.f
@@ -72,7 +72,8 @@
      &               'dust/gas','dustVol/H','<a>[mic]',
      &               ('Jstar('//trim(nuc_nam(j))//')',j=1,NNUC),
      &               ('Nstar('//trim(nuc_nam(j))//')',j=1,NNUC),
-     &               'vd0','vd1','vd2','vd3','vdrift0','log(rhodust)'
+     &               'vd0','vd1','vd2','vd3','vdrift0',
+     &               'log(rhodust)', 'log(bmix)'
 
       !---------------------------------------
       ! ***  write total column densities  ***
@@ -219,7 +220,8 @@
      &      (LOG10(MAX(1.Q-300, Jst(j))),j=1,NNUC), 
      &      (MIN(999999.99999,Nst(j)),j=1,NNUC),
      &      vdrift(0:3),vdrift0,
-     &      (LOG10(MAX(1.Q-300,rhodust)))
+     &      (LOG10(MAX(1.Q-300,rhodust))),
+     &      (LOG10(MAX(1.Q-300,bmix(ip))))
         amax = max(amax,amean)
       enddo 
 
